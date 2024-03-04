7520225708a6c26178c099b3e691a56783817eb5
Salfiskin <103762253+SalfisKin@users.noreply.github.com>
2024-03-03 23:59:06 -0500

    Player animation added

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -13,13 +13,14 @@ public class PlayerScript : MonoBehaviour
 
     private float horizontalMovement;
     public ChatGPTScript chatGPTScript;
-
+    private Animator player_Animator; // Reference to the Animator component
 
     private Rigidbody2D m_Rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        player_Animator = GetComponent<Animator>(); // Initialize the Animator component
     }
 
     // Update is called once per frame
@@ -29,6 +30,12 @@ public class PlayerScript : MonoBehaviour
         {
             horizontalMovement = Input.GetAxisRaw("Horizontal");
             m_Rigidbody.velocity = new Vector2(horizontalMovement * speed, m_Rigidbody.velocity.y);
+
+            // Check if the player is moving by comparing horizontal movement to zero
+            bool isRunning_right = (horizontalMovement > 0);
+            player_Animator.SetBool("Is_running_right", isRunning_right); // Set the Animator's isRunning parameter
+            bool isRunning_left = (horizontalMovement < 0);
+            player_Animator.SetBool("Is_running_left", isRunning_left); // Set the Animator's isRunning parameter
         }
 
         if (meetNPC & npcName != null)
@@ -37,6 +44,7 @@ public class PlayerScript : MonoBehaviour
             {
                 freeze = true;
                 chatGPTScript.SendReply(npcName);
+                player_Animator.SetBool("Is_running", false);
             }
         }
 
